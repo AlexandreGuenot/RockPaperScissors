3ae32cbf801f6278ab832cd69893ff591d8fb6e2
Alex Guenot <aguenot75@gmail.com>
2022-09-23 17:25:15 +0200

    Rock Paper Scissors working in console

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # JS-BasicRockPaperScissors
+
+
+        else if (playerSelection === "scissors" && computerSelection === "rock"){
+            Declared_Winner = "You loose ! "+playerSelection+" beats"+computerSelection+"!"
+        }
+        else if (playerSelection === "scissors" && computerSelection === "paper"){
+            Declared_Winner = "You win ! "+playerSelection+" beats"+computerSelection+"!"
+        }
+        else if  (playerSelection === "paper" && computerSelection === "rock"){
+            Declared_Winner = "You win ! "+playerSelection+" beats"+computerSelection+"!"
+        }
+        else if  (playerSelection === "paper" && computerSelection === "scissors"){
+            Declared_Winner = "You loose ! "+playerSelection+" beats"+computerSelection+"!"
+        }
+        else if  (playerSelection === "rock" && computerSelection === "scissors"){
+            Declared_Winner = "You win ! "+playerSelection+" beats"+computerSelection+"!"
+        }
+        else if  (playerSelection === "rock" && computerSelection === "paper"){
+            Declared_Winner = "You loose ! "+playerSelection+" beats"+computerSelection+"!"
+        }
+
+
+
+
+items[Math.floor(Math.random()*items.length)]
---
+++ myscript.js
@@ -0,0 +1,65 @@
+function getComputerChoice(){
+    let items = Array("rock","paper","scissors")
+    let computer_move = items[Math.floor(Math.random()*items.length)]
+    return computer_move
+}
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    RoundWinner = ""
+        if (playerSelection === computerSelection ){
+            RoundWinner = "It's a draw ! "
+        }
+        else if (playerSelection === "scissors" && computerSelection === "rock"){
+            RoundWinner = "Computer wins"
+        }
+        else if (playerSelection === "scissors" && computerSelection === "paper"){
+            RoundWinner = "Player wins"
+        }
+        else if  (playerSelection === "paper" && computerSelection === "rock"){
+            RoundWinner = "Player wins"
+        }
+        else if  (playerSelection === "paper" && computerSelection === "scissors"){
+            RoundWinner = "Computer wins"
+        }
+        else if  (playerSelection === "rock" && computerSelection === "scissors"){
+            RoundWinner = "Player wins"
+        }
+        else if  (playerSelection === "rock" && computerSelection === "paper"){
+            RoundWinner = "Computer wins"
+        }
+        return RoundWinner
+    }
+
+function game(){
+    let player_count=0
+    let computer_count=0
+    for (let i = 0; i<5;i++){
+        playerSelection = prompt("Rock, Paper, Scissors : ")
+        computerSelection = getComputerChoice()
+        RoundWinner = playRound(playerSelection, computerSelection)
+        console.log("Round "+(i+1) + " : "+RoundWinner)
+        if(RoundWinner === "Player wins"){
+            player_count = player_count+1
+            console.log("Player : "+ player_count +" Computer :  " + computer_count)
+        }
+        else if(RoundWinner === "Computer wins"){
+            computer_count = computer_count+1
+            console.log("Player : "+ player_count +" Computer :  " + computer_count)
+        }
+        else{
+            console.log("Player : "+ player_count +" Computer :  " + computer_count)
+        }
+    }
+    if(player_count > computer_count){
+        console.log("The player wins the Game ! ")
+    }
+    else if(player_count < computer_count){
+        console.log("The Computer Wins the Game :( ")
+    }
+    else{
+        console.log("It's a draw ")
+    }
+}
+
+game()
