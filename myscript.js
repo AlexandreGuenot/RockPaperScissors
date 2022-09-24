6b3cd51ebd7a05391e8c5e74ff51cb6de865131f
Alex Guenot <aguenot75@gmail.com>
2022-09-24 23:11:48 +0200

    Result In

--- myscript.js
+++ myscript.js
@@ -1,79 +1,81 @@
+let player_count=0
+let computer_count=0
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) =>{
+    button.addEventListener('click', ()=> {
+        playRound(button.id, getComputerChoice())
+    })
+})
+
+const results = document.querySelector('.result')
+const player = document.querySelector('#player_choice')
+const computer = document.querySelector('#computer_choice')
+const winner = document.querySelector('#winner')
+
+results.appendChild(player);
+results.appendChild(computer);
+results.appendChild(winner);
+
+const box_score = document.querySelector('.box_score')
+const my_score = document.querySelector('#player_box')
+const computer_score = document.querySelector('#computer_box')
+
+box_score.appendChild(my_score);
+box_score.appendChild(computer_score);
+
 function getComputerChoice(){
-    let items = Array("rock","paper","scissors")
+    let items = Array("ROCK","PAPER","SCISSORS")
     let computer_move = items[Math.floor(Math.random()*items.length)]
     return computer_move
 }
 
 function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.toUpperCase();
     RoundWinner = ""
         if (playerSelection === computerSelection ){
             RoundWinner = "It's a draw ! "
-            console.log("Player: ",playerSelection, " ", "Computer : ",computerSelection + " It's a draw !")
+            player.innerHTML = "Player: "+playerSelection+"<br />"
+            computer.innerHTML = "Computer : "+computerSelection +"<br />";
+            winner.innerHTML = RoundWinner;
+            
+                                    
         }
-        else if (playerSelection === "scissors" && computerSelection === "rock"){
+        else if (playerSelection === "SCISSORS" && computerSelection === "ROCK"){
             RoundWinner = "Computer wins"
-            console.log("Player: ",playerSelection, " ", "Computer : ",computerSelection + " Computer wins")
+            player.innerHTML = "Player: "+playerSelection+"<br />"
+            computer.innerHTML = "Computer : "+computerSelection+"<br />";
+            winner.innerHTML = RoundWinner;
         }
-        else if (playerSelection === "scissors" && computerSelection === "paper"){
+        else if (playerSelection === "SCISSORS" && computerSelection === "PAPER"){
             RoundWinner = "Player wins"
-            console.log("Player: ",playerSelection, " ", "Computer : ",computerSelection + " Player wins")
+            player.innerHTML = "Player: "+playerSelection+"<br />"
+            computer.innerHTML = "Computer : "+computerSelection+"<br />"
+            winner.innerHTML = RoundWinner; 
         }
-        else if  (playerSelection === "paper" && computerSelection === "rock"){
+        else if  (playerSelection === "PAPER" && computerSelection === "ROCK"){
             RoundWinner = "Player wins"
-            console.log("Player: ",playerSelection, " ", "Computer : ",computerSelection + " Player wins")
+            player.innerHTML = "Player: "+playerSelection+"<br />"
+            computer.innerHTML = "Computer : "+computerSelection+"<br />"
+            winner.innerHTML = RoundWinner;
         }
-        else if  (playerSelection === "paper" && computerSelection === "scissors"){
+        else if  (playerSelection === "PAPER" && computerSelection === "SCISSORS"){
             RoundWinner = "Computer wins"
-            console.log("Player: ",playerSelection, " ", "Computer : ",computerSelection + " Computer wins")
+            player.innerHTML = "Player: "+playerSelection+"<br />"
+            computer.innerHTML = "Computer : "+computerSelection +"<br />"
+            winner.innerHTML = RoundWinner;
+
         }
-        else if  (playerSelection === "rock" && computerSelection === "scissors"){
+        else if  (playerSelection === "ROCK" && computerSelection === "SCISSORS"){
             RoundWinner = "Player wins"
-            console.log("Player: ",playerSelection, " ", "Computer : ",computerSelection + " Player wins")
+            player.innerHTML = "Player: "+playerSelection+"<br />"
+            computer.innerHTML = "Computer : "+computerSelection +"<br />"
+            winner.innerHTML = RoundWinner;
         }
-        else if  (playerSelection === "rock" && computerSelection === "paper"){
+        else if  (playerSelection === "ROCK" && computerSelection === "PAPER"){
             RoundWinner = "Computer wins"
-            console.log("Player: ",playerSelection, " ", "Computer : ",computerSelection + " Computer wins")
+            player.innerHTML = "Player: "+playerSelection+"<br />"
+            computer.innerHTML = "Computer : "+computerSelection +"<br />"
+            winner.innerHTML = RoundWinner;
         }
         return RoundWinner
-    }
-   
-function game(){
-    let player_count=0
-    let computer_count=0
-    const buttons = document.querySelectorAll('button');
-
-    buttons.forEach((button) =>{
-        button.addEventListener('click', ()=> {
-            playRound(button.id, getComputerChoice())
-        })
-    })
-    /*for (let i = 0; i<5;i++){
-        playerSelection = prompt("Rock, Paper, Scissors : ")
-        computerSelection = getComputerChoice()
-        RoundWinner = playRound(playerSelection, computerSelection)
-        console.log("Round "+(i+1) + " : "+RoundWinner)
-        if(RoundWinner === "Player wins"){
-            player_count = player_count+1
-            console.log("Player : "+ player_count +" Computer :  " + computer_count)
-        }
-        else if(RoundWinner === "Computer wins"){
-            computer_count = computer_count+1
-            console.log("Player : "+ player_count +" Computer :  " + computer_count)
-        }
-        else{
-            console.log("Player : "+ player_count +" Computer :  " + computer_count)
-        }
-    }
-    if(player_count > computer_count){
-        console.log("The player wins the Game ! ")
-    }
-    else if(player_count < computer_count){
-        console.log("The Computer Wins the Game :( ")
-    }
-    else{
-        console.log("It's a draw ")
-    }*/
 }
-
-game()
