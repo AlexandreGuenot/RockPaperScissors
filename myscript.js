0d9be7ff61084dfd504339f374da933841396b93
Alex Guenot <aguenot75@gmail.com>
2022-09-25 02:09:46 +0200

    All Done

--- myscript.js
+++ myscript.js
@@ -6,7 +6,7 @@ buttons.forEach((button) =>{
         playRound(button.id, getComputerChoice())
     })
 })
-
+const choices = document.querySelector('.choices')
 const results = document.querySelector('.result')
 const player = document.querySelector('#player_choice')
 const computer = document.querySelector('#computer_choice')
@@ -19,6 +19,8 @@ results.appendChild(winner);
 const box_score = document.querySelector('.box_score')
 const my_score = document.querySelector('#player_box')
 const computer_score = document.querySelector('#computer_box')
+my_score.innerHTML = player_count                   
+computer_score.innerHTML = computer_count 
 
 box_score.appendChild(my_score);
 box_score.appendChild(computer_score);
@@ -33,36 +35,61 @@ function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toUpperCase();
     RoundWinner = ""
         if (playerSelection === computerSelection ){
-            RoundWinner = "It's a draw ! "
+            RoundWinner = "It's a draw"
             player.innerHTML = "Player: "+playerSelection+"<br />"
             computer.innerHTML = "Computer : "+computerSelection +"<br />";
             winner.innerHTML = RoundWinner;
-            
-                                    
+            my_score.innerHTML = player_count                   
+            computer_score.innerHTML = computer_count                   
         }
         else if (playerSelection === "SCISSORS" && computerSelection === "ROCK"){
             RoundWinner = "Computer wins"
             player.innerHTML = "Player: "+playerSelection+"<br />"
             computer.innerHTML = "Computer : "+computerSelection+"<br />";
             winner.innerHTML = RoundWinner;
+            computer_count+=1;
+            my_score.innerHTML = player_count                   
+            computer_score.innerHTML = computer_count
+            if(computer_count == 5){
+                results.textContent = 'The Computer wins the game'
+            }
+               
         }
         else if (playerSelection === "SCISSORS" && computerSelection === "PAPER"){
             RoundWinner = "Player wins"
             player.innerHTML = "Player: "+playerSelection+"<br />"
             computer.innerHTML = "Computer : "+computerSelection+"<br />"
             winner.innerHTML = RoundWinner; 
+            player_count+=1; 
+            my_score.innerHTML = player_count                   
+            computer_score.innerHTML = computer_count   
+            if(player_count == 5){
+                results.textContent = 'You win the game'
+            }  
         }
         else if  (playerSelection === "PAPER" && computerSelection === "ROCK"){
             RoundWinner = "Player wins"
             player.innerHTML = "Player: "+playerSelection+"<br />"
             computer.innerHTML = "Computer : "+computerSelection+"<br />"
             winner.innerHTML = RoundWinner;
+            player_count+=1;
+            my_score.innerHTML = player_count                   
+            computer_score.innerHTML = computer_count   
+            if(player_count == 5){
+                results.textContent = 'You win the game'
+            } 
         }
         else if  (playerSelection === "PAPER" && computerSelection === "SCISSORS"){
             RoundWinner = "Computer wins"
             player.innerHTML = "Player: "+playerSelection+"<br />"
             computer.innerHTML = "Computer : "+computerSelection +"<br />"
             winner.innerHTML = RoundWinner;
+            computer_count+=1;
+            my_score.innerHTML = player_count                   
+            computer_score.innerHTML = computer_count    
+            if(computer_count == 5){
+                results.textContent = 'The Computer wins the game'
+            }
 
         }
         else if  (playerSelection === "ROCK" && computerSelection === "SCISSORS"){
@@ -70,12 +97,28 @@ function playRound(playerSelection, computerSelection){
             player.innerHTML = "Player: "+playerSelection+"<br />"
             computer.innerHTML = "Computer : "+computerSelection +"<br />"
             winner.innerHTML = RoundWinner;
+            player_count+=1; 
+            my_score.innerHTML = player_count                   
+            computer_score.innerHTML = computer_count 
+            if(player_count == 5){
+                results.textContent = 'You win the game'
+            }  
         }
         else if  (playerSelection === "ROCK" && computerSelection === "PAPER"){
             RoundWinner = "Computer wins"
             player.innerHTML = "Player: "+playerSelection+"<br />"
             computer.innerHTML = "Computer : "+computerSelection +"<br />"
             winner.innerHTML = RoundWinner;
+            computer_count+=1; 
+            my_score.innerHTML = player_count                   
+            computer_score.innerHTML = computer_count   
+            if(computer_count == 5){
+                results.textContent = 'The Computer wins the game'
+            }
         }
         return RoundWinner
+    }
+
+function game(){
+    playRound
 }
